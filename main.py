51000fb51e154ff06a350a43df840d0b590a0d6b
hossein_sadeghi <hskhabbaz@gmail.com>
2021-05-04 22:26:04 +0430

    just push

--- main.py
+++ main.py
@@ -4,33 +4,34 @@ import os
 
 if __name__ == '__main__':
     os.system('cls||clear')
-    width = 400
-    height = 300
+    width = 600
+    height = 500
 
-    display = pygame.display.set_mode((width,height))
+    window = pygame.display.set_mode((width,height))
     clock = pygame.time.Clock()
+    print('display in main', window)
 
-    snake = snake.Snake(width, height, display)   
+    s = snake.Snake(width, height, window)   
 
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    snake.x_change = -1
-                    snake.y_change = 0
+                    s.x_change = -1
+                    s.y_change = 0
                 elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    snake.x_change = 1
-                    snake.y_change = 0
+                    s.x_change = 1
+                    s.y_change = 0
                 elif event.key == pygame.K_w or event.key == pygame.K_UP:
-                    snake.x_change = 0
-                    snake.y_change = 1
+                    s.x_change = 0
+                    s.y_change = 1
                 elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    snake.x_change = 0
-                    snake.y_change = -1
-        snake.move()
-        snake.show()
+                    s.x_change = 0
+                    s.y_change = -1
+        s.move()
 
-        display.fill((0, 255, 0))
+        window.fill((0, 255, 0))
         pygame.display.update()
-        clock.tick(60)
+        s.show()
+        clock.tick(30)
--- snake.py
+++ snake.py
@@ -2,7 +2,7 @@ import pygame
 
 class Snake:
 
-    def __init__(self, width, height, d):
+    def __init__(self, width, height, display):
         self.w = 16
         self.h = 16
         self.x = width / 2
@@ -10,13 +10,14 @@ class Snake:
         self.name = 'Hossein'
         self.color = (0, 127, 0)
         self.speed = 5
-        self.score = 0
-        self.d = d
+        self.score= 0
+        self.display = display
         self.x_change = 0
         self.y_change = 0
 
     def show(self):
-        pygame.draw.rect(self.d, self.color, [self.x, self.y, self.w, self.h])
+        print('display in snake class',self.display)
+        pygame.draw.rect(self.display, self.color, [self.x, self.y, self.w, self.h])
     
     def move(self):
         if self.x_change == -1:
